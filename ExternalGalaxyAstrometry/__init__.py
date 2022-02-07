b18f68ce439c6bf68c217359d33c1114b8661bc8
Paul McMillan <paul@astro.lu.se>
2022-02-07 16:23:44 +0100

    docstring

--- ExternalGalaxyAstrometry/__init__.py
+++ ExternalGalaxyAstrometry/__init__.py
@@ -1,2 +1,3 @@
+'''Tools to analyse data with proper motions under the assumption that they are drawn from an external rotating disc galaxy.'''
 from ._AngleConversions import *
 from ._GeometricFitting import findGradientsParametersFromData
