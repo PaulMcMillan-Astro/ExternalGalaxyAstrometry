d27d6d2332d25fbd82f7caf37eecb0fd9e3968c9
Paul McMillan <paul@astro.lu.se>
2022-02-08 11:07:48 +0100

    Bit of lint

--- ExternalGalaxyAstrometry/_AngleConversions.py
+++ ExternalGalaxyAstrometry/_AngleConversions.py
@@ -1,36 +1,38 @@
 import numpy as np
+import pandas as pd
 
-a0vdM = 78.76 
+a0vdM = 78.76
 '''a0 from van der Marel & Kallivayalil (2014) average of H I values'''
-d0vdM = -69.19 
+d0vdM = -69.19
 '''d0 from van der Marel & Kallivayalil (2014) average of H I values'''
 
 
+def Spherical2Orthonormal(alphadeg, deltadeg,
+                          mualphastar=None, mudelta=None,
+                          mualphastar_error=None, mudelta_error=None,
+                          mualpha_mudelta_corr=None,
+                          alpha0deg=a0vdM, delta0deg=d0vdM):
+    """Converts angular position, proper motion, and proper motion uncertainty
+    to convenient local form
 
-
-
-def Spherical2Orthonormal(alphadeg,deltadeg,
-                          mualphastar=None,mudelta=None,
-                          mualphastar_error=None,mudelta_error=None, mualpha_mudelta_corr=None,
-                          alpha0deg=a0vdM,delta0deg=d0vdM) :
-    """Converts angular position, proper motion, and proper motion uncertainty to convenient local form 
-    
-    Input positions given in degrees, output in radians. 
+    Input positions given in degrees, output in radians.
     Proper motion in mas/yr (though I suppose pm units are unimportant).
-    Conversion is to orthonormal x,y coordinates following eqs. 2 from DR2 demo paper
-    
+    Conversion is to orthonormal x,y coordinates following eqs. 2 from DR2
+    demo paper
+
     Uncertainties do not include any uncertainty in position.
-    
-    Default alpha0, delta0 values (centre) from van der Marel & Kallivayalil (2014) 
-    
-    Outputs: 
-        
+
+    Default alpha0, delta0 values (centre) from van der Marel 
+    & Kallivayalil (2014) 
+
+    Outputs:
         x,y: angular positions
         mux,muy: proper motions in x,y directions (if given as input)
-        mux_error,muy_error,mux_muy_corr: uncertainties & correlation coefficient in proper motions (if given as input)
+        mux_error,muy_error,mux_muy_corr: uncertainties & correlation 
+            coefficient in proper motions (if given as input)
     """
-    dalpharad = np.deg2rad(alphadeg-alpha0deg)
     
+    dalpharad = np.deg2rad(alphadeg-alpha0deg)
     cosda = np.cos(dalpharad)
     sinda = np.sin(dalpharad)
     deltarad = np.deg2rad(deltadeg)
@@ -42,12 +44,13 @@ def Spherical2Orthonormal(alphadeg,deltadeg,
 
     x = cosd*sinda
     y = sind*cosd0 - cosd*sind0*cosda
-    if mualphastar is None or mudelta is None :
-        return x,y
-    if mualphastar_error is None or mudelta_error is None or mualpha_mudelta_corr is None : 
+    if mualphastar is None or mudelta is None:
+        return x, y
+    if mualphastar_error is None or mudelta_error is None \
+        or mualpha_mudelta_corr is None:
         mux = cosda*mualphastar-sind*sinda*mudelta
         muy = sind0*sinda*mualphastar+(cosd*cosd0+sind*sind0*cosda)*mudelta
-        return x,y,mux,muy
+        return x, y, mux, muy
 
     CovarianceMatrix = np.zeros((len(mualphastar_error),2,2))
     CovarianceMatrix[:,0,0] = mualphastar_error**2
@@ -79,19 +82,28 @@ def Spherical2Orthonormal(alphadeg,deltadeg,
     return x,y,mux,muy,mux_error,muy_error,mux_muy_corr
 
 
+def Spherical2Orthonormal_pandas(data,alpha0deg=a0vdM,delta0deg=d0vdM,convertUncertainties=True) :
+    if convertUncertainties :
+        return Spherical2Orthonormal(data.ra,data.dec, data.pmra, data.pmdec, 
+                                    data.pmra_error,data.pmdec_error,data.pmra_pmdec_corr,
+                                    alpha0deg=alpha0deg,delta0deg=delta0deg)
+    else :
+        return Spherical2Orthonormal(data.ra,data.dec, data.pmra, data.pmdec,
+                                    alpha0deg=alpha0deg,delta0deg=delta0deg)
+    
+
 def Orthonormal2Spherical(x,y,
                           mux=None,muy=None,
                           mux_error=None,muy_error=None, mux_muy_corr=None,
                           alpha0deg=a0vdM,delta0deg=d0vdM) :
     """Converts angular position and proper motion from convenient local form 
-    
+
     Input positions in radians, outputs in degrees. Proper motion in mas/yr (though I suppose pm units are unimportant).
     Conversion is to orthonormal x,y coordinates following eqs. 2 from DR2 demo paper
-    
+
     Default alpha0, delta0 values (centre) from van der Marel & Kallivayalil (2014) 
     """
-    
-    
+
     lambda0 = np.deg2rad(alpha0deg)
     phi0 = np.deg2rad(delta0deg)
     rho = np.sqrt(x**2+y**2)
@@ -100,13 +112,14 @@ def Orthonormal2Spherical(x,y,
     
     tanLambda = x*sinC/(rho*cosC*np.cos(phi0)-y*sinC*np.sin(phi0))
     sinPhi = cosC*np.sin(phi0) + y*sinC*np.cos(phi0)/rho
-    alphadeg,deltadeg = np.rad2deg(lambda0+np.arctan(tanLambda)),np.rad2deg(np.arcsin(sinPhi))
+    alphadeg, deltadeg = np.rad2deg(lambda0+np.arctan(tanLambda)), \
+        np.rad2deg(np.arcsin(sinPhi))
 
-    if mux is None or muy is None :
-        return alphadeg,deltadeg
+    if mux is None or muy is None:
+        return alphadeg, deltadeg
 
     dalpharad = np.deg2rad(alphadeg-alpha0deg)
-    
+
     cosda = np.cos(dalpharad)
     sinda = np.sin(dalpharad)
     deltarad = np.deg2rad(deltadeg)
@@ -117,12 +130,12 @@ def Orthonormal2Spherical(x,y,
     sind0 = np.sin(delta0rad)
 
     # Not really a rotation matrix.
-    RotationMatrix = np.zeros((len(mux),2,2))
-    RotationMatrix[:,0,0] = cosda
-    RotationMatrix[:,0,1] = -sind*sinda
-    RotationMatrix[:,1,0] = sind0*sinda
-    RotationMatrix[:,1,1] = (cosd*cosd0+sind*sind0*cosda)
-    
+    RotationMatrix = np.zeros((len(mux), 2, 2))
+    RotationMatrix[:, 0, 0] = cosda
+    RotationMatrix[:, 0, 1] = -sind*sinda
+    RotationMatrix[:, 1, 0] = sind0*sinda
+    RotationMatrix[:, 1, 1] = (cosd*cosd0+sind*sind0*cosda)
+
     Determinant = RotationMatrix[:,0,0]*RotationMatrix[:,1,1]-RotationMatrix[:,0,1]*RotationMatrix[:,1,0]
     # If it was really a rotation matrix the inverse would just be the transpose.
     InverseRotationMatrix = np.zeros((len(mux),2,2))
@@ -169,7 +182,8 @@ def Orthonormal2Internal(x,y,mux,muy,
 
     N.B. Error in eq. B.21 of DR2 paper, which is now corrected in this code
 
-    Inputs: 
+    Parameters
+    ----------
         x,y: angular positions
         mux,muy: proper motions in x,y directions (if given as input)
         vx,vy,vz: Bulk velocity of LMC (units: mas/yr). vz is line-of-sight so conversion is at assumed distance
@@ -177,7 +191,8 @@ def Orthonormal2Internal(x,y,mux,muy,
         mux_error,muy_error,mux_muy_corr: uncertainties & correlation coefficient in proper motions (if given as input)
 
 
-    Outputs: 
+    Returns
+    -------
         R (radians), phi (radians), vR (mas/yr), vt (mas/yr),
         (if mux_error, muy_error, mux_mu_corr given as input, also:)
         vR_error (mas/yr), vt_error (mas/yr), 
--- ExternalGalaxyAstrometry/__init__.py
+++ ExternalGalaxyAstrometry/__init__.py
@@ -1,3 +1,115 @@
 '''Tools to analyse data with proper motions under the assumption that they are drawn from an external rotating disc galaxy.'''
 from ._AngleConversions import *
 from ._GeometricFitting import findGradientsParametersFromData
+
+
+def filterUsingProperMotions(data,a0=a0vdM,d0=d0vdM,
+                             adjustForCorrelations= True,
+            defaultParallaxCut = 5., # parallax/parallax_error < defaultParallaxCut
+            defaultPMChiSq = 9.21, # chi square for proper motions. 9.21 correponds to 99% confidence interval
+            defaultMagnitudeCut = 20., # Magnitude cut for final sample
+            defaultMagnitudeCutInitialFilter = 19., # Magnitude cut for initial sample
+            defaultRadiusCutInitialFilter = np.sin(np.deg2rad(3)) # Radius cut for initial sample
+                ) :
+    '''This function needs a lot of explaining'''
+    x,y,mux,muy = Spherical2Orthonormal_pandas(data,alpha0deg=a0,delta0deg=d0,convertUncertainties=False)
+    # mask on parallax/parallax_error
+    maskparallaxLMC = ((data.parallax)/(data.parallax_error) < defaultParallaxCut)
+    # mask on default radius cut (restrict to LMC dominated region)
+    maskposition =  ((x**2 + y**2 < defaultRadiusCutInitialFilter*defaultRadiusCutInitialFilter))
+    # mask anything too faint (again, reduces contamination because these have large uncertainties)
+    maskbrightishLMC = data.phot_g_mean_mag < defaultMagnitudeCutInitialFilter
+
+    maskpickpmLMC = maskparallaxLMC & maskbrightishLMC & maskposition
+
+    pmxLMC0,pmyLMC0, findMedianRobustCovariance(mux[maskpickpmLMC],muy[maskpickpmLMC])
+    pmxLMC0 = np.median()
+    pmyLMC0 = np.median(muy[maskpickpmLMC])
+
+
+    # find Robust scatter estimate (RSE)
+    # RSE is 0.39 * [(90th percentile) - (10th percentile)]
+    # For a Gaussian it is the same the standard deviation (but it is more robust to outliers)
+
+    percentilesx = np.percentile(data[maskpickpmLMC]['mux'],[10,90])
+    percentilesy = np.percentile(data[maskpickpmLMC]['muy'],[10,90])
+    RSEx = 0.390152*(percentilesx[1]-percentilesx[0])
+    RSEy = 0.390152*(percentilesy[1]-percentilesy[0])
+    maskPMRSE = ((np.absolute(data[maskpickpmLMC]['mux']-pmxLMC0)<4*RSEx) &
+                (np.absolute(data[maskpickpmLMC]['muy']-pmyLMC0)<4*RSEy) )
+    pmxcov = (data[maskpickpmLMC]['mux'])[maskPMRSE]
+    pmycov = (data[maskpickpmLMC]['muy'])[maskPMRSE]
+    pmcov = np.row_stack((pmxcov,pmycov))
+    covar = np.cov(pmcov)
+    icovar = np.linalg.inv(covar)
+
+    print('Covarience matrix for proper motions (x & y)')
+    print (covar)
+
+    dpmx = np.array(data['mux']-pmxLMC0)
+    dpmy = np.array(data['muy']-pmyLMC0)
+    maskpmLMC = ( (dpmx*dpmx*icovar[0][0] + 2*dpmx*dpmy*icovar[0][1] + dpmy*dpmy*icovar[1][1]) < defaultPMChiSq ) 
+
+    pmxcov, pmycov, pmcov, dpmx, dpmy = [],[],[],[], []
+
+    print(len(data[maskpmLMC]),' stars fit initial proper motion cuts')
+    maskposition =  ((data['x']*data['x'] + data['y']*data['y'] < defaultRadiusCutInitialFilter*defaultRadiusCutInitialFilter))
+    maskmag = data['phot_g_mean_mag'] < defaultMagnitudeCutInitialFilter
+    maskmyLMC = (maskparallaxLMC & maskpmLMC) & maskposition & maskmag
+    median_parallax = np.median(np.array(data[maskmyLMC]['parallax']))
+
+    print('median parallax of stars in LMC:', median_parallax, 'mas')
+
+    pmra_decorr = ( np.array(data.pmra) + 
+                np.array(data.parallax_pmra_corr)*np.array(data.pmra_error)/np.array(data.parallax_error)
+                * (median_parallax-np.array(data.parallax)) )
+
+    pmdec_decorr = ( np.array(data.pmdec) + 
+                np.array(data.parallax_pmdec_corr)*np.array(data.pmdec_error)/np.array(data.parallax_error)
+                * (median_parallax-np.array(data.parallax)) )
+
+    data['pmra_decorr'] = pmra_decorr
+    data['pmdec_decorr'] = pmdec_decorr
+
+    #np.array(data.pmdec)
+    _,_,LLmux,LLmuy = Spherical2Orthonormal(np.array(data.ra),np.array(data.dec),
+                                                np.array(data.pmra_decorr),np.array(data.pmdec_decorr))
+
+    data['mux_decorr'] = LLmux
+    data['muy_decorr'] = LLmuy
+
+    pmra_decorr, pmdec_decorr = [],[]
+    LLmux,LLmuy = [],[],[],[]
+
+    pmxLMC0_decorr = np.median(data[maskpickpmLMC]['mux_decorr'])
+    pmyLMC0_decorr = np.median(data[maskpickpmLMC]['muy_decorr'])
+
+
+    # find RSE
+    percentilesx = np.percentile(data[maskpickpmLMC]['mux_decorr'],[10,90])
+    percentilesy = np.percentile(data[maskpickpmLMC]['muy_decorr'],[10,90])
+    RSEx = 0.390152*(percentilesx[1]-percentilesx[0])
+    RSEy = 0.390152*(percentilesy[1]-percentilesy[0])
+    maskPMRSE = ((np.absolute(data[maskpickpmLMC]['mux_decorr']-pmxLMC0)<4*RSEx) & 
+                (np.absolute(data[maskpickpmLMC]['muy_decorr']-pmyLMC0)<4*RSEy))
+    pmxcov = (data[maskpickpmLMC]['mux_decorr'])[maskPMRSE]
+    pmycov = (data[maskpickpmLMC]['muy_decorr'])[maskPMRSE]
+        #plt.hist(dpma,np.arange(-3,3,0.1),normed=True)
+        #plt.plot(np.arange(-3,3,0.1),Gau(np.arange(-3,3,0.1),0,np.sqrt(0.214)),'--')
+    pmcov = np.row_stack((pmxcov,pmycov))
+    covar_decorr = np.cov(pmcov)
+    icovar_decorr = np.linalg.inv(covar_decorr)
+
+
+    print('Covarience matrix for proper motions (corrected x & y)')
+    print (covar_decorr)
+
+    dpmx_decorr = np.array(data['mux_decorr']-pmxLMC0_decorr)
+    dpmy_decorr = np.array(data['muy_decorr']-pmyLMC0_decorr)
+    maskpmLMC_decorr = ( (dpmx_decorr*dpmx_decorr*icovar_decorr[0][0] + 2*dpmx_decorr*dpmy_decorr*icovar_decorr[0][1] 
+                + dpmy_decorr*dpmy_decorr*icovar_decorr[1][1]) < defaultPMChiSq ) 
+    pmxcov, pmycov, pmcov, dpmx, dpmy = [],[],[],[], []
+    maskMagnitudeFinal = (data['phot_g_mean_mag'] < defaultMagnitudeCut)
+    maskLMCfinal = maskparallaxLMC & maskpmLMC_decorr & maskMagnitudeFinal
+
+    print(len(data[maskLMCfinal]),'stars in final sample')
